a10978cf42544c3db2d65130526e846037830301
StanislavPB <perminovsb@gmail.com>
2024-04-08 19:44:51 +0100

    add Jpa repository

--- src/main/java/org/jpaspringdata/repository/ToDoRepository.java
+++ src/main/java/org/jpaspringdata/repository/ToDoRepository.java
@@ -1,9 +1,12 @@
 package org.jpaspringdata.repository;
 
 
+import lombok.experimental.PackagePrivate;
 import org.jpaspringdata.entity.ToDoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,5 +17,15 @@ public interface ToDoRepository extends JpaRepository<ToDoEntity, Integer> {
 
     public List<ToDoEntity> findByName(String name);
     public List<ToDoEntity> findByDescription(String description);
+
+    default List<ToDoEntity> findToDoWithLongNames(int minLength){
+        return findAll().stream()
+                .filter(toDoEntity -> toDoEntity.getName() != null && toDoEntity.getName().length() > minLength)
+                .toList();
+    }
+
+
+    @Query("SELECT t FROM ToDoEntity t WHERE LENGTH(t.name) > 10 AND t.id > :id")
+    public List<ToDoEntity> findToDoWithNameLengthLongerThanAndIdGreaterThan(@Param("id") Integer id);
 
 }
